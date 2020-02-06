4497ffbe9d413b53c876f1e3a55d7f929c2c4261
agustinjauslin <60743705+agustinjauslin@users.noreply.github.com>
2020-02-06 11:54:26 -0300

    ejercicio 2
    cosole.log, concatenar en js

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,8 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre = prompt("Please enter your name", "Harry Potter");//prompt regresa algo, debe tener necesariamente el igual para tomar un valor y lo guarda en la variable
+	console.log("tu nombre es "+nombre);//console log es un echo que te muestras los resultados en la CONSOLA
+										//para concatenar en js se utiliza en +
 }
 
