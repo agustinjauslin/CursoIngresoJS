f339fe28002dbc30dd5e24f03c76d863d1247e27
agustinjauslin <60743705+agustinjauslin@users.noreply.github.com>
2020-02-08 12:33:31 -0300

    clase e sabado
    repaso

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,108 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var objetoUno;
+	var objetoDos;
+	var objetoTres;
+	var suma;
+
+
+	objetoUno=document.getElementById('PrecioUno').value;
+	objetoUno=parseInt(objetoUno);
+
+
+
+	objetoDos=document.getElementById('PrecioDos').value;
+	objetoDos=parseInt(objetoDos);
+	
+
+
+	objetoTres=document.getElementById('PrecioTres').value;
+	objetoTres=parseInt(objetoTres);
+
+
+
+
+	suma=objetoUno+objetoDos+objetoTres;
+
+	console.log(suma);
 	
 }
 function Promedio () 
 {
+	var objetoUno;
+	var objetoDos;
+	var objetoTres;
+	var suma;
+	var promedio;
+
+
+	objetoUno=document.getElementById('PrecioUno').value;
+	objetoUno=parseInt(objetoUno);
+
+
+
+	objetoDos=document.getElementById('PrecioDos').value;
+	objetoDos=parseInt(objetoDos);
+	
+
+
+	objetoTres=document.getElementById('PrecioTres').value;
+	objetoTres=parseInt(objetoTres);
+
+
+
+
+	suma=objetoUno+objetoDos+objetoTres;
+
+
+	promedio=suma/3;
+
+	console.log(promedio);
+
+
+
+
 	
 }
+
+
+
+
+
+
+
+
 function PrecioFinal () 
 {
+	var objetoUno;
+	var objetoDos;
+	var objetoTres;
+	var suma;
+	var iva;
+
+
+	objetoUno=document.getElementById('PrecioUno').value;
+	objetoUno=parseInt(objetoUno);
+
+
+
+	objetoDos=document.getElementById('PrecioDos').value;
+	objetoDos=parseInt(objetoDos);
+	
+
+
+	objetoTres=document.getElementById('PrecioTres').value;
+	objetoTres=parseInt(objetoTres);
+
+
+
+
+	suma=objetoUno+objetoDos+objetoTres;
+
+
+	iva=suma+suma*0.21;
+
+	console.log(iva);
 	
 }
--- 8-TPs/jsFerretePinturas.js
+++ 8-TPs/jsFerretePinturas.js
@@ -2,12 +2,47 @@
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
-function FahrenheitCentigrados () 
+function FahrenheitCentigrados () //C=(F-32).(5/9)
 {
+	var temperatura;
+	var resta;
+	var division;
+	var resultadoFinal;
+
+	temperatura=document.getElementById('Temperatura').value;
+	temperatura = parseInt(temperatura);
+
+	resta=temperatura-32;
+	division=5/9;
+	resultadoFinal=resta*division;
+	alert(resultadoFinal);
+
+
+
 	
 }
 
-function CentigradosFahrenheit () 
+function CentigradosFahrenheit () //F=C.(9/5)+32
 {
+	var temperatura;
+	var division;
+	var resultadoFinal;
+
+
+
+
+	temperatura=document.getElementById('Temperatura').value;
+	temperatura = parseInt(temperatura);
+
+
+
+	division=9/5;
+	resultadoFinal=temperatura*division+32;
+
+
+	alert(resultadoFinal);
+
+
+
 	
 }
