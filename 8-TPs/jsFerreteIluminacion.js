270786332087caedce8ea22a890ac10e502e7a1e
agustinjauslin <unzquadatr@gmail.com>
2020-02-25 22:22:24 +0100

    25 2 owo

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -1,112 +1,101 @@
 /*4.	Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
-B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento 
-del 40 % y si es de otra marca el descuento es del 30%.
-C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace 
-un descuento del 25 % y si es de otra marca el descuento es del 20%.
-D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, 
-si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% 
-de ingresos brutos en informar del impuesto con el siguiente mensaje:
+B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
+C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
 function CalcularPrecio () 
-{	//VARIABLES
-	var cantidad;//cantidad
-	var marca;//marcas seleccionada
-	var lamparaValor;//valor estandar 35$
-	var precioBruto;
-	var PrecioFinal;
-	var descuento;
-	//VALORES
-	cantidad=document.getElementById('Cantidad').value;
-	cantidad=parseInt(cantidad);
-	marca=document.getElementById('Marca').value;
-	lamparaValor=35;
-	//CALCULOS
-	precioBruto=lamparaValor*cantidad;
-
-if (cantidad>5)
 {
-		descuento=50;
-}
-else
-{
-		if (cantidad==5) 
-		{
-				if (marca=="ArgentinaLuz") 
+ 	var cantlamparitas;
+ 	var descuento;
+ 	var precioBruto;
+ 	var Valorlampara;
+ 	var preciofinal;
+ 	var marca;
+ 	
+ 	
+
+	Valorlampara=35;
+	
+
+	cantlamparitas=document.getElementById('Cantidad').value;
+ 	cantlamparitas=parseInt(cantlamparitas);
+
+ 	marca=document.getElementById('Marca').value;
+
+ 	precioBruto=cantlamparitas*Valorlampara;
+ 	
+	if (cantlamparitas>5) 
+ 	{
+ 		descuento=50;
+ 		
+ 	}else
+ 	{   
+ 		if (cantlamparitas==5) 
+		{ 
+											//NO PONGO LAS CUENTAS EN CADA UNO PORQUE SIEMPRE SON LAS MISMAS, DONDE CAMBIA SOLO ES EN EL VALOR DEL DESCUENTO, ENTONCES PONGO LA CUENTA DEL DESCUENTO AFUERA Y ESTABLEZCO EL DESCUENTO EN CADA UNO Y YA ESTA.
+			if (marca=="ArgentinaLuz") 
+			{
+				descuento=40;
+ 	
+			}else
+			{ descuento=30;
+
+			}
+ 		}
+ 		else 
+ 		{
+ 		  if (cantlamparitas==4) 
+		   {
+				if (marca=="ArgentinaLuz"|| marca=="FelipeLamparas") 
 				{
-						descuento=40;
-				}
-				else
+					descuento=25;
+				
+				}else
 				{
-						descuento=30;
+					descuento=20;
 				}
+
+		   }
+		   else
+ 		    {
+ 				if (cantlamparitas==3) 
+				{
+					if (marca=="ArgentinaLuz") 
+					{
+						descuento=15;
+					}
+					else
+					{
+							if (marca=="FelipeLamparas") 
+							{
+								descuento=10;
+					
+							}else
+							{
+								descuento=5;
+							}
+					}
+				}
+ 				else
+ 				{
+ 					descuento=0;
+ 				}
+ 			
+ 			}
+ 				
 		}
-		else
-		{
-				if (cantidad==4)
-				{
-						if ((marca=="ArgentinaLuz")||(marca=="FelipeLamparas"))
-						{
-								descuento=25;
-						}
-						else
-						{
-								descuento=20
-						}
+ 	}
 
+	
+ 	preciofinal=precioBruto-(precioBruto*(descuento/100));
+	document.getElementById('precioDescuento').value = preciofinal;
 
-						
-				
-				
-				}		
-				else
-				{
-
-						if (cantidad==3)
-						{
-								if (marca=="ArgentinaLuz") 
-								{
-										descuento=15;
-								}
-								else
-								{
-										if (marca=="FelipeLamparas") 
-										{
-												descuento=10;
-										}
-										else
-										{
-												descuento=5;
-										}
-
-								
-								}
-
-
-
-						
-						
-						
-						}
-						else
-						{
-								descuento=0;
-						}
-				
-				}
-		
-		}
+ 	
 }
-precioFinal=precioBruto-precioBruto*(descuento/100);
-document.getElementById('precioDescuento').value=precioFinal;
-
-
-}
-
-
 
 
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,147 @@
+/*
+	Realizar el algoritmo que permita el ingreso por prompt de las notas 
+	(validar entre 0 y 10) , el sexo (validar el sexo “f” o “m”) de 5 
+	alumnos, informar por alert:
+		a) El promedio de las notas totales.
+		b) La nota más baja y el sexo de esa persona.(min)
+		c) La cantidad de varones que su nota haya sido mayor o igual a 6.(contador con condiciones)
+*/
 function mostrar()
 {
+	//VARIABLES
+	var nota1;
+	var nota2;
+	var nota3;
+	var sexo;
+	var suma;
+	var supersuma;
+	supersuma=0;
+	var contador;
+	contador=0;
+	var valorMinimo;
+	valorMinimo=20;
+	var sexominimo;
+	var promedio;
+	var promediototal;
+	promediototal=0;
+	var contadorvarones;
+	contadorvarones=0;
+	var promediovarones;
+	//VARIABLES Y WHILE
+	while(contador<5)
+	{
+						nota1=prompt("ingrese nota 1");
+						nota1=parseInt(nota1);
+						while(!(nota1>-1 && nota1<11))
+						{
+							nota1=prompt("debe ingresar una nota entre 0 y 10");
+							nota1=parseInt(nota1);		
+						}
+						nota2=prompt("ingrese nota 2");+
+						nota2=parseInt(nota2);
+						while(!(nota2>-1 && nota2<11))
+						{
+								nota2=prompt("debe ingresar una nota entre 0 y 10");
+								nota2=parseInt(nota2);
+						}
+						nota3=prompt("ingrese nota 3");
+						nota3=parseInt(nota3);
+						while(!(nota3>-1&&nota3<11))
+						{
+							nota3=prompt("debe ingresar una nota entre 0 y 10");
+							nota3=parseInt(nota3);
+						}
+						sexo=prompt("ingrese el sexo del alumno");
+						while(!(sexo=='m'||sexo=='f'))
+						{
+							sexo=prompt("ingrese un sexo valido(con la letra m para masculino o la letra f para femenino)");
+						}
+						suma=nota1+nota2+nota3;
 
-}
+
+
+
+
+
+
+						// 3    1    0
+
+													//   n1  n2   n3
+						//minimo PRIMERA VEZ    6   5    4   
+						if (contador==0) 
+						{
+							if (nota1<nota2&&nota1<nota3) 
+							{
+									valorMinimo=nota1;
+									sexominimo=sexo;
+
+							}
+							else
+							{
+									if (nota2<nota1&&nota2<nota3) 
+									{
+											valorMinimo=nota2;
+											sexominimo=sexo;	
+									}
+									else
+									{
+											if (nota3<nota2&&nota3<nota1) 
+											{		
+													valorMinimo=nota3;
+													sexominimo=sexo;
+											}
+									}	
+							}				
+						}
+						else
+						{
+							/*//valor minimo 4//20??????
+
+													 	 n1  n2   n3
+						//maximo y minimo PRIMERA VEZ    9    8    7     */
+								if (nota1<valorMinimo&&nota1<nota2&&nota1<nota3) 
+								{
+										valorMinimo=nota1;
+										sexominimo=sexo;
+								}
+								else
+								{
+										if (nota2<valorMinimo&&nota2<nota1&&nota2<nota3) 
+										{
+												valorMinimo=nota2;
+												sexominimo=sexo;
+										}
+										else
+										{
+												if (nota3<valorMinimo&&nota3<nota1&&nota3<nota2) 
+												{
+														valorMinimo=nota3;
+														sexominimo=sexo;
+												}
+												else
+												{
+														valorMinimo=valorMinimo;
+														
+												}	
+										}
+								}
+
+								
+									
+						}
+						//tomo el valor minimo y luego lo comparo con el proximo alumno	
+						promediovarones=suma/3;
+						promediovarones=parseInt(promediovarones);
+						if (promediovarones>5 && sexo=='m') 
+						{
+							contadorvarones++;
+						}
+						promedio=suma/3;//promedio alumno1
+						promediototal=promediototal+promedio;
+						contador++;
+	}
+	promediototal=promediototal/contador
+	alert("a.el promedio de todos los alumnos es de :"+promediototal);
+	alert("b.el valor minimo es "+valorMinimo+" y el sexo de esa persona es "+sexominimo);
+	alert("c.los HOMBRES MACHOS SUPERIORES son: "+contadorvarones);
+	}
