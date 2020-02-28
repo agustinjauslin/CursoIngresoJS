e044e9b712458003996833460bcb8ea66d47b390
alumno <alumno>
2020-02-28 11:56:07 -0300

    parcial

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,136 @@
+/*
+a.de la temp mas alta el nombre y el sexo
+b. si tiene mas de 38 de temperatura y es un niño(<12) o mayor de edad(>60)mensaje "riesgo" con nombre
+c.temperatura del mas joven/del mas viejo
+d.promedio de edad/total y por mujeres
+*/
+
 function mostrar()
 {
+	var nombre;
+	var nombreMaxTemp;
+	var edad;
+	var sexo;
+	var sexoMaxTemp;
+	var temperatura;
+	var temperaturaJoven;
+	var temperaturaViejo;
+	
+	var tempMax;
+	var respuesta;
+	var contador;
+	contador=0;
+	var promedio;
+	var suma;
+	suma=0;
+	var flag;
+	flag=true;
+	respuesta=true;
+	var promedioMujeres;
+	var sumaMujeres;
+	
+	var contadorMujeres;
+	contadorMujeres=0;
+	temperaturaViejo=0;
+	temperaturaJoven=0;
+	sumaMujeres=0;
+	//	promedioMujeres=sumaMujeres/contadorMujeres
 
+
+	while(respuesta==true)
+	{
+			nombre=prompt("ingrese su nombre");
+			while(!(isNaN(nombre)))
+			{
+					nombre=prompt("ingrese su nombre");
+			}
+
+
+			sexo=prompt("ingrese su sexo");
+			while(sexo!='m'&&sexo!='f')
+			{
+					sexo=prompt("reingrese su sexo");
+			}
+
+
+			edad=prompt("ingrese su edad");
+			edad=parseInt(edad);
+			while(edad<1||edad>100)
+			{
+					edad=prompt("reingrese su edad");
+					edad=parseInt(edad);
+			}
+
+
+			temperatura=prompt("ingrese su temperatura corporal");
+			temperatura=parseInt(temperatura);
+			while(temperatura<34||temperatura>42)
+			{
+					temperatura=prompt("ingrese su temperatura corporal");
+					temperatura=parseInt(temperatura);
+			}
+
+
+			if(flag)
+			{
+				tempMax=temperatura;
+				nombreMaxTemp=nombre;
+				sexoMaxTemp=sexo;
+
+
+				temperaturaJoven=temperatura;
+				temperaturaViejo=temperatura;
+
+				
+				edadJoven=edad;
+				edadViejo=edad;
+				flag=false;
+
+			}
+			else
+			{
+					if (temperatura>tempMax) 
+					{
+							tempMax=temperatura;
+							nombreMaxTemp=nombre;
+							sexoMaxTemp=sexo;
+
+					}
+					if (edad<edadJoven) 
+					{
+							temperaturaJoven=temperatura;
+
+					}
+					if(edad>edadViejo)
+					{
+							temperaturaViejo=temperatura;
+					}
+			}
+
+
+
+			if (edad<12||edad>60 &&temperatura>38) 
+			{
+					alert("esta en riesgo "+nombre);
+			}
+
+			if (sexo=='f') 
+			{
+					contadorMujeres++;
+					sumaMujeres=sumaMujeres+edad;
+			}
+
+			suma=suma+edad;
+
+
+			contador++;
+			respuesta=confirm("ingrese aceptar para continuar");
+
+	}
+	promedio=suma/contador;
+	promedioMujeres=sumaMujeres/contadorMujeres
+
+	document.write("a. la persona con maxima temperatura es "+nombreMaxTemp+" y su sexo es "+sexoMaxTemp+"<br>");
+	document.write("b. la temperatura del mas joven es de "+temperaturaJoven+" y la temperatura del mas viejo es "+temperaturaViejo+"<br>");
+	document.write("c.el promedio general de edad es "+promedio+" y el promedio de mujeres es de :"+promedioMujeres+"<br>")
 }
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,149 @@
+/*
+Para la gestión de un hotel,
+ingresar los siguientes datos validados de una reserva
+nombre del huésped
+cantidad de personas 
+cantidad de dia de estadia 
+forma de pago(efectivo , tarjeta o QR)
+
+informar el huésped que trajo más personas en una sola reserva.
+la cantidad de personas que se quedaron más días(comparar y ver quien se queda mas dias)
+la forma de pago más utilizada.
+el promedio de cantidad de días por reserva
+*/
 function mostrar()
 {
+var nombre;
+var cantidadHuespedes;
+var diasEstadia;
+var formaPago;
+var respuesta;
+var cantidadMax;
+var nombreCantidadMax;
+var contadorEfectivo;
+var contadorTarjeta;
+var contadorQR;
+var contadorReserva;
+var sumaReserva;
+var flag;
+
+flag=true;
+contadorReserva=0;
+sumaReserva=0;
+contadorQR=0;
+contadorTarjeta=0;
+contadorEfectivo=0;
+respuesta=true;
+
+while(respuesta)
+{
+	nombre=prompt("ingrese su nombre");
+	while(!(isNaN(nombre)))
+	{
+		nombre=prompt("reingrese su nombre");
+	}
+
+	cantidadHuespedes=prompt("ingrese la cantidad de huespedes");
+	cantidadHuespedes=parseInt(cantidadHuespedes);
+	while(cantidadHuespedes<1)
+	{	
+			cantidadHuespedes=prompt("reingrese la cantidad de huespedes");
+			cantidadHuespedes=parseInt(cantidad);
+	}
+
+	diasEstadia=prompt("ingrese la cantidad de dias");
+	diasEstadia=parseInt(diasEstadia);
+	while(diasEstadia<1)
+	{
+			diasEstadia=prompt("reingrese la cantidad de dias");
+			diasEstadia=parseInt(diasEstadia);
+	}
+	formaPago=prompt("ingrese la forma de pago");
+	while(formaPago!='efectivo'&&formaPago!='tarjeta'&&formaPago!='QR')
+	{
+			formaPago=prompt("reingrese la forma de pago");	
+	}
+	
+	if (flag==true) 
+	{		//informar el huésped que trajo más personas en una sola reserva.
+			cantidadMax=cantidadHuespedes;
+			nombreCantidadMax=nombre;
+			//la cantidad de personas que se quedaron más días(comparar y ver quien se queda mas dias)
+			cantidadMaxDias=diasEstadia;
+			cantidadHuespedesMaxDias=cantidadHuespedes;
+			flag=false;
+	}
+	else
+	{		//informar el huésped que trajo más personas en una sola reserva.
+			if (cantidadHuespedes>cantidadMax) 
+			{
+				cantidadMax=cantidadHuespedes;
+				nombreCantidadMax=nombre;	
+			}//la cantidad de personas que se quedaron más días(comparar y ver quien se queda mas dias)
+			if (diasEstadia>cantidadMaxDias) 
+			{
+					cantidadMaxDias=diasEstadia;
+					cantidadHuespedesMaxDias=cantidadHuespedes;
+			}
+	}
+	//contador de formas de pago
+	if (formaPago=='tarjeta') 
+	{
+		contadorTarjeta++;
+	}
+	else
+	{
+			if (formaPago=='efectivo') 
+			{
+				contadorEfectivo++;
+			}
+			else
+			{
+				contadorQR++;
+			}
+	}
+	contadorReserva++;
+	sumaReserva=sumaReserva+diasEstadia;
+	respuesta=confirm("presione cont para ingresar mas datos")
+	
+
+}//salgo del while
+
+
+
+	//el promedio de cantidad de días por reserva
+
+
+	//comparacuon de formas de pago
+	if (contadorEfectivo>contadorTarjeta&&contadorEfectivo>contadorQR) 
+	{
+			formaDePagoMasUtilizada='efectivo'
+	}
+	else
+	{
+			if (contadorTarjeta>contadorEfectivo&&contadorTarjeta>contadorQR) 
+			{
+					formaDePagoMasUtilizada='tarjeta';
+			}
+			else
+			{
+					formaDePagoMasUtilizada='QR';
+			}
+	}
+	promedioCantidadDias=sumaReserva/contadorReserva;
+
+
+/*
+informar el huésped que trajo más personas en una sola reserva.
+la cantidad de personas que se quedaron más días(comparar y ver quien se queda mas dias)
+la forma de pago más utilizada.
+el promedio de cantidad de días por reserva
+*/
+
+document.write("a. el huesped que trajo mas personas es "+nombreCantidadMax+"<br>");
+document.write("b. la cantidad de personas que se quedaron mas dias son "+cantidadMaxDias+"<br>")
+document.write("c. la fomrma de pago mas utilizada es "+formaDePagoMasUtilizada+"<br>");
+document.write("d.el promedio de cantidad de dias es de "+promedioCantidadDias);
+
 
 }
